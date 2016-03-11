f9ec1fab78f47282626403fa92120a2f36e07761
Kucher-Max <kuchermax@ukr.net>
2016-03-11 15:12:23 +0200

    HW 09.03.16

--- Task 2.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$nums = array(12, 15, 20, 25, 59, 79);
-echo array_sum($nums)/count($nums);
-
-?>
--- the_last_hero.php
+++ the_last_hero.php
@@ -1,153 +1,153 @@
 <?php
 $participants = [
     ['name' => 'Nikita',
-        'surname' => 'Lysenko',
-        'age' => 32
+     'surname' => 'Lysenko',
+     'age' => 32
     ],
 
     ['name' => 'Dima',
-        'surname' => 'Polishchuk',
-        'age' => 28
+     'surname' => 'Polishchuk',
+     'age' => 28
     ],
 
     ['name' => 'Alex',
-        'surname' => 'Shevchuk',
-        'age' => 25
+     'surname' => 'Shevchuk',
+     'age' => 25
     ],
 
     ['name' => 'Sergey',
-        'surname' => 'Oliynyk',
-        'age' => 22
+     'surname' => 'Oliynyk',
+     'age' => 22
     ],
 
     ['name' => 'Vlad',
-        'surname' => 'Kravchenko',
-        'age' => 30
+     'surname' => 'Kravchenko',
+     'age' => 30
     ],
 
     ['name' => 'Andrey',
-        'surname' => 'Tkachenko',
-        'age' => 29
+     'surname' => 'Tkachenko',
+     'age' => 29
     ],
 
     ['name' => 'Artem',
-        'surname' => 'Bondarenko',
-        'age' => 24
+     'surname' => 'Bondarenko',
+     'age' => 24
     ],
 
     ['name' => 'Ivan',
-        'surname' => 'Kovalchuk',
-        'age' => 26
+     'surname' => 'Kovalchuk',
+     'age' => 26
     ],
 
     ['name' => 'Anton',
-        'surname' => 'Kovalenko',
-        'age' => 31
+     'surname' => 'Kovalenko',
+     'age' => 31
     ],
 
     ['name' => 'Maxim',
-        'surname' => 'Shevchenko',
-        'age' => 28
+     'surname' => 'Shevchenko',
+     'age' => 28
     ],
 
     ['name' => 'Oleg',
-        'surname' => 'Boyko',
-        'age' => 27
+     'surname' => 'Boyko',
+     'age' => 27
     ],
 
     ['name' => 'Roman',
-        'surname' => 'Melnyk',
-        'age' => 22
+     'surname' => 'Melnyk',
+     'age' => 22
     ],
 
     ['name' => 'Vlad',
-        'surname' => 'Shevchenko',
-        'age' => 23
+     'surname' => 'Shevchenko',
+     'age' => 23
     ],
 
     ['name' => 'Nikita',
-        'surname' => 'Melnyk',
-        'age' => 21
+     'surname' => 'Melnyk',
+     'age' => 21
     ],
 
     ['name' => 'Dima',
-        'surname' => 'Shevchenko',
-        'age' => 26
+     'surname' => 'Shevchenko',
+     'age' => 26
     ],
 
     ['name' => 'Alex',
-        'surname' => 'Boyko',
-        'age' => 37
+     'surname' => 'Boyko',
+     'age' => 37
     ],
 
     ['name' => 'Vlad',
-        'surname' => 'Melnyk',
-        'age' => 29
+     'surname' => 'Melnyk',
+     'age' => 29
     ],
 
     ['name' => 'Sergey',
-        'surname' => 'Kovalenko',
-        'age' => 28
+     'surname' => 'Kovalenko',
+     'age' => 28
     ],
 
     ['name' => 'Vlad',
-        'surname' => 'Bondarenko',
-        'age' => 23
+     'surname' => 'Bondarenko',
+     'age' => 23
     ],
 
     ['name' => 'Andrey',
-        'surname' => 'Lysenko',
-        'age' => 27
+     'surname' => 'Lysenko',
+     'age' => 27
     ],
 
     ['name' => 'Artem',
-        'surname' => 'Polishchuk',
-        'age' => 24
+     'surname' => 'Polishchuk',
+     'age' => 24
     ],
 
     ['name' => 'Ivan',
-        'surname' => 'Shevchenko',
-        'age' => 30
+     'surname' => 'Shevchenko',
+     'age' => 30
     ],
 
     ['name' => 'Anton',
-        'surname' => 'Shevchuk',
-        'age' => 22
+     'surname' => 'Shevchuk',
+     'age' => 22
     ],
 
     ['name' => 'Maxim',
-        'surname' => 'Oliynyk',
-        'age' => 28
+     'surname' => 'Oliynyk',
+     'age' => 28
     ],
 
     ['name' => 'Oleg',
-        'surname' => 'Kovalenko',
-        'age' => 26
+     'surname' => 'Kovalenko',
+     'age' => 26
     ],
 
     ['name' => 'Roman',
-        'surname' => 'Melnyk',
-        'age' => 25
+     'surname' => 'Melnyk',
+     'age' => 25
     ],
 
     ['name' => 'Vlad',
-        'surname' => 'Tkachenko',
-        'age' => 19
+     'surname' => 'Tkachenko',
+     'age' => 19
     ],
 
     ['name' => 'Dima',
-        'surname' => 'Bondarenko',
-        'age' => 33
+     'surname' => 'Bondarenko',
+     'age' => 33
     ],
 
     ['name' => 'Nikita',
-        'surname' => 'Kovalchuk',
-        'age' => 24
+     'surname' => 'Kovalchuk',
+     'age' => 24
     ],
 
     ['name' => 'Andrey',
-        'surname' => 'Shevchenko',
-        'age' => 35
+     'surname' => 'Shevchenko',
+     'age' => 35
     ],
 ];
 echo "<pre>";

0ac2c6b77ff4ffe566a58c2d7809a1023306372c
Kucher-Max <kuchermax@ukr.net>
2016-03-12 13:58:52 +0200

    HW 09.03.16 complete

--- the_last_hero.php
+++ the_last_hero.php
@@ -1,162 +1,20 @@
 <?php
+//Part 1//
+$name = ["Nikita","Dima","Alex","Sergey","Vlad","Andrey","Artem","Ivan","Anton","Maxim","Oleg","Roman"];
+$surname = ["Melnyk","Shevchenko","Boyko","Kovalenko","Bondarenko","Tkachenko","Kovalchuk","Kravchenko",
+    "Oliynyk","Shevchuk","Polishchuk","Lysenko"];
 
-$participants = [
-    ['name' => 'Nikita',
-     'surname' => 'Lysenko',
-     'age' => 32
-    ],
-
-    ['name' => 'Dima',
-     'surname' => 'Polishchuk',
-     'age' => 28
-    ],
-
-    ['name' => 'Alex',
-     'surname' => 'Shevchuk',
-     'age' => 25
-    ],
-
-    ['name' => 'Sergey',
-     'surname' => 'Oliynyk',
-     'age' => 22
-    ],
-
-    ['name' => 'Vlad',
-     'surname' => 'Kravchenko',
-     'age' => 30
-    ],
-
-    ['name' => 'Andrey',
-     'surname' => 'Tkachenko',
-     'age' => 29
-    ],
-
-    ['name' => 'Artem',
-     'surname' => 'Bondarenko',
-     'age' => 24
-    ],
-
-    ['name' => 'Ivan',
-     'surname' => 'Kovalchuk',
-     'age' => 26
-    ],
-
-    ['name' => 'Anton',
-     'surname' => 'Kovalenko',
-     'age' => 31
-    ],
-
-    ['name' => 'Maxim',
-     'surname' => 'Shevchenko',
-     'age' => 28
-    ],
-
-    ['name' => 'Oleg',
-     'surname' => 'Boyko',
-     'age' => 27
-    ],
-
-    ['name' => 'Roman',
-     'surname' => 'Melnyk',
-     'age' => 22
-    ],
-
-    ['name' => 'Vlad',
-     'surname' => 'Shevchenko',
-     'age' => 23
-    ],
-
-    ['name' => 'Nikita',
-     'surname' => 'Melnyk',
-     'age' => 21
-    ],
-
-    ['name' => 'Dima',
-     'surname' => 'Shevchenko',
-     'age' => 26
-    ],
-
-    ['name' => 'Alex',
-     'surname' => 'Boyko',
-     'age' => 37
-    ],
-
-    ['name' => 'Vlad',
-     'surname' => 'Melnyk',
-     'age' => 29
-    ],
-
-    ['name' => 'Sergey',
-     'surname' => 'Kovalenko',
-     'age' => 28
-    ],
-
-    ['name' => 'Vlad',
-     'surname' => 'Bondarenko',
-     'age' => 23
-    ],
-
-    ['name' => 'Andrey',
-     'surname' => 'Lysenko',
-     'age' => 27
-    ],
-
-    ['name' => 'Artem',
-     'surname' => 'Polishchuk',
-     'age' => 24
-    ],
-
-    ['name' => 'Ivan',
-     'surname' => 'Shevchenko',
-     'age' => 30
-    ],
-
-    ['name' => 'Anton',
-     'surname' => 'Shevchuk',
-     'age' => 22
-    ],
-
-    ['name' => 'Maxim',
-     'surname' => 'Oliynyk',
-     'age' => 28
-    ],
-
-    ['name' => 'Oleg',
-     'surname' => 'Kovalenko',
-     'age' => 26
-    ],
-
-    ['name' => 'Roman',
-     'surname' => 'Melnyk',
-     'age' => 25
-    ],
-
-    ['name' => 'Vlad',
-     'surname' => 'Tkachenko',
-     'age' => 19
-    ],
-
-    ['name' => 'Dima',
-     'surname' => 'Bondarenko',
-     'age' => 33
-    ],
-
-    ['name' => 'Nikita',
-     'surname' => 'Kovalchuk',
-     'age' => 24
-    ],
-
-    ['name' => 'Andrey',
-     'surname' => 'Shevchenko',
-     'age' => 35
-    ],
-];
-
-shuffle($participants);
-
-echo "<pre>";
+for ($i = 1; $i <= 30; $i++){
+    $nameNew = array_rand($name, 1);
+    $surnameNew = array_rand($surname, 1);
+    $age = rand(18, 60);
+    $participants [$i] = ["name" => $name[$nameNew], "surname" => $surname[$surnameNew], "age" => $age];
+}
+echo '<pre>';
 print_r($participants);
+echo '</pre>';
 
+//Part 2//
 shuffle($participants);
 
 foreach($participants as $key => $values){
@@ -169,3 +27,4 @@ foreach($participants as $key => $values){
 }
 
 ?>
+

948736792951f8bdb503f249bef700bac8b8bcd4
Kucher-Max <kuchermax@ukr.net>
2016-03-12 21:45:46 +0200

    HW 11.03.16

---
+++ hw 11.03.16/Task 13.php
@@ -0,0 +1,21 @@
+<?php
+
+$var1= [2,3,4,5,6,7,8,9];
+foreach ($var1 as $key=> $value) {echo "2 * ".$value.=" = ".(2*$value)."<br>";}
+echo '<br>';
+foreach ($var1 as $key=> $value) {echo "3 * ".$value.=" = ".(3*$value)."<br>";}
+echo '<br>';
+foreach ($var1 as $key=> $value) {echo "4 * ".$value.=" = ".(4*$value)."<br>";}
+echo '<br>';
+foreach ($var1 as $key=> $value) {echo "5 * ".$value.=" = ".(5*$value)."<br>";}
+echo '<br>';
+foreach ($var1 as $key=> $value) {echo "6 * ".$value.=" = ".(6*$value)."<br>";}
+echo '<br>';
+foreach ($var1 as $key=> $value) {echo "7 * ".$value.=" = ".(7*$value)."<br>";}
+echo '<br>';
+foreach ($var1 as $key=> $value) {echo "8 * ".$value.=" = ".(8*$value)."<br>";}
+echo '<br>';
+foreach ($var1 as $key=> $value) {echo "9 * ".$value.=" = ".(9*$value)."<br>";}
+echo '<br>';
+
+?>
